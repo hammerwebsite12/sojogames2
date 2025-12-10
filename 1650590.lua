b24bc3d9884b78c2a374880caa1931943d3302ee
hammerwebsite12 <thepoorergamer2424@gmail.com>
2025-12-11 06:42:53 +0800

    Upload via drag-and-drop tool

---
+++ 1650590.lua
@@ -0,0 +1,17 @@
+-- 1650590's Lua and Manifest Created by Morrenus
+-- Nightmare Frames
+-- Created: October 15, 2025 at 09:04:17 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(1650590) -- Nightmare Frames
+-- MAIN APP DEPOTS
+addappid(1650591, 1, "b391089e934afc01f17809ff5055cd3a2aa2b480135666d1b29088457b400ffe") -- Depot 1650591
+setManifestid(1650591, "3557506409151358638", 2699497699)
+-- DLCS WITH DEDICATED DEPOTS
+-- Nightmare Frames - Portrait artbook  OST (AppID: 2016250)
+addappid(2016250)
+addappid(2016250, 1, "b080b4ca7dad1470071944690a32c42155ee7ca41b8457c4eb0c913b4635fdbe") -- Nightmare Frames - Portrait artbook  OST - Depot 2016250
+setManifestid(2016250, "5920227366659494865", 196276449)
