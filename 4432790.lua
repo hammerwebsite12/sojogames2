12e5eccf81610f6a681ef009739d89aadfaf9c23
hammerwebsite12 <223803886+hammerwebsite12@users.noreply.github.com>
2026-08-18 02:05:13 +0800

    Add files via upload

---
+++ 4432790.lua
@@ -0,0 +1,12 @@
+-- 4432790's Lua and Manifest Created by Hubcap Manifest
+-- Cosmic Crusher
+-- Created: August 17, 2026 at 12:47:11 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(4432790) -- Cosmic Crusher
+-- MAIN APP DEPOTS
+addappid(4432791, 1, "ffe1314e46a6d37d509a0a488544ae9fb5dff718b7df1d58091667beef5c1d53") -- Depot 4432791
+setManifestid(4432791, "8580018216908893270", 216315117)
