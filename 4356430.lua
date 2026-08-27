9346e23d75ca42699526ad4cb67d2f05e983ee9f
hammerwebsite12 <223803886+hammerwebsite12@users.noreply.github.com>
2026-08-27 10:20:57 +0800

    Update 4356430.lua

--- 4356430.lua
+++ 4356430.lua
@@ -1,24 +1,12 @@
--- 4356430's Lua and Manifest Created by Hubcap Manifest
--- NBA 2K27
--- Created: August 26, 2026 at 13:12:22 EDT
--- Website: https://hubcapmanifest.com/
--- Total Depots: 3
--- Total DLCs: 4
--- Shared Depots: 1
-
--- MAIN APPLICATION
-addappid(4356430) -- NBA 2K27
+addappid(4356430)
 addtoken(4356430, "13716260120817756824")
--- MAIN APP DEPOTS
-addappid(4356431, 1, "a1ccdda87579dfa269b9015cc7ba18b7eb8339848b5eb408bfea8bdfb31eb8a4") -- Depot 4356431
+addappid(4356431, 1, "a1ccdda87579dfa269b9015cc7ba18b7eb8339848b5eb408bfea8bdfb31eb8a4")
 setManifestid(4356431, "4426313921998626225", 80455180238)
-addappid(4687050, 1, "c96a0954f3836680afb31e3b837546bb92606ccaebcc3e1bf8cd3761b8221c25") -- Depot 4687050
+addappid(4687050, 1, "c96a0954f3836680afb31e3b837546bb92606ccaebcc3e1bf8cd3761b8221c25")
 setManifestid(4687050, "4767089068737929486", 29646686473)
--- SHARED DEPOTS (from other apps)
-addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358")
 setManifestid(228988, "6645201662696499616", 29212173)
--- DLCS WITHOUT DEDICATED DEPOTS
-addappid(4357560) -- NBA 2K27 Pro Pass Season 1
-addappid(4357570) -- NBA 2K27 Hall of Fame Pass Season 1
-addappid(4357580) -- NBA 2K27 MyCAREER Bonus Offer Season 1
-addappid(4357590) -- NBA 2K27 MyTEAM Bonus Offer Season 1
+addappid(4357560)
+addappid(4357570)
+addappid(4357580)
+addappid(4357590)
